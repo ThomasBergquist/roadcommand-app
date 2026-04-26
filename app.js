3bc5232931834ff59ae26ebda9ca47744eaecba4
ThomasBergquist <86751641+ThomasBergquist@users.noreply.github.com>
2026-04-26 00:45:52 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -2070,7 +2070,7 @@ async function fetchFuelPrice(region) {
 
   const seriesId = PADD[region] || PADD['Unknown'];
   const isStatLevel = false;
-  const url = 'https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=value&facets[series][]=' + seriesId + '&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=1&api_key=2kWPj1CuJO5R9mve6S0C45KtGxk8HGpSFE3EiXGF';
+  const url = 'https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=value&facets[series][]=' + seriesId + '&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=1&api_key=DEMO_KEY';
 
   try {
     const r = await fetch(url);
